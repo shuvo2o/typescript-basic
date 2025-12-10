7175ba8b28429ea4578aef140069b06aebf00d99
shuvo2o <eng.shuvo03@gmail.com>
2025-12-10 18:21:57 +0600

    basic

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// function 
+function greet(name) {
+    return "Hello " + name;
+}
+console.log(greet('Shuvo'));
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+// function 
+function greet(name:string): string{
+    return "Hello " + name;
+}
+console.log(greet('Shuvo'))
