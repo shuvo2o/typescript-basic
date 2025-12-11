67fccd84109a096ddb6dcef6810987306290a533
shuvo2o <eng.shuvo03@gmail.com>
2025-12-11 10:49:09 +0600

    default params done

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,12 @@
 "use strict";
 // function 
-function greet(fname, lname) {
+function greet(fname, lname, title) {
     return `Hello, ${fname}-${lname}`;
 }
 console.log(greet('Shuvo', 'Das'));
+// default params
+function welcome(name = "Guest") {
+    return `Welcome ${name}`;
+}
+console.log(welcome('Shuvo'));
+console.log(welcome());
