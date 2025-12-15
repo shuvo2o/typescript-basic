a5aadfd9d9cfa33d60770191c1bb119d54bff4e1
shuvo2o <eng.shuvo03@gmail.com>
2025-12-15 10:14:43 +0600

    blog post project

---
+++ dist/blog.js
@@ -0,0 +1,12 @@
+"use strict";
+function showBlogPost(post) {
+    console.log(`The Blogs title is ${post.title} and its makers is ${post.author} and its content ${post.content}, its published on ${post.publishedDate}. Ther similar tags for find book ${post.tags} tags`);
+}
+const blogpost1 = {
+    title: "Learn Typescript",
+    author: "Shuvo Dichi",
+    content: "This is dummy text",
+    publishedDate: "15-12-25",
+    tags: ["Learning", "TS", "JS alternative"]
+};
+showBlogPost(blogpost1);
