699861eb478088daea9bd260fd2c0c0ca98649fb
shuvo2o <eng.shuvo03@gmail.com>
2025-12-19 10:11:43 +0600

    generics called with interfaces

--- src/len-generics.ts
+++ src/len-generics.ts
@@ -3,3 +3,14 @@ function printLength<T extends { length: number }>(item: T) {
 }
 printLength("Hello Shuvo");
 printLength([2,4,7,8]);
+
+
+interface HasId{
+    id:number
+}
+
+function getId<T extends HasId>(item:T){
+    return item.id;
+}
+const result = getId({id: 40})
+console.log("The id is:", result)
